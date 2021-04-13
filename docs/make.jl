a33a81a8e024de7b6820711b2f00cd881dd21910
xzackli <xzackli@gmail.com>
2021-04-13 19:42:22 -0400

    edit link and footer

--- docs/make.jl
+++ docs/make.jl
@@ -12,8 +12,8 @@ makedocs(;
         prettyurls=get(ENV, "CI", "false") == "true",
         canonical="https://xzackli.github.io/PSPipePlanckRender.jl",
         assets=String[],
-        edit_link=nothing,
-        footer="Markdown rendering of the [PSPipe](https://github.com/simonsobs/PSpipe) Planck project."
+        # edit_link=nothing,
+        footer="Markdown rendering of the [PSPipe](https://github.com/simonsobs/PSpipe) Planck project using [Literate](https://fredrikekre.github.io/Literate.jl/) and [Documenter](https://juliadocs.github.io/Documenter.jl/dev/)."
     ),
     pages=[
         "Home" => "index.md",
